a7bd9756bd1df03aff2ccd5cdcc4458e24514243
Khaldon <khaldon@Khaldons-MacBook-Pro.local>
2022-10-06 09:12:10 +0200

    réglages

--- controller/settings.php
+++ controller/settings.php
@@ -1,3 +1,10 @@
 <?php
-echo $twig->render('navbar.html.twig', ['login' => $_SESSION['login']]);
-echo $twig->render('settings.html.twig');
+if (isset($_SESSION['login'])) {
+    echo $twig->render('navbar.html.twig', ['login' => $_SESSION['login']]);
+    echo $twig->render('settings.html.twig');
+} else {
+    $erreur = "Veuillez vous connecter pour avoir accès à cette fonctionnalité";
+    echo $twig->render('navbar.html.twig');
+    echo $twig->render('erreur.html.twig', ['erreur' => $erreur]);
+    echo $twig->render('users.html.twig');
+}
--- controller/signin.php
+++ controller/signin.php
@@ -1,3 +1,8 @@
 <?php
-echo $twig->render('navbar.html.twig');
-echo $twig->render('users.html.twig');
+if (!isset($_SESSION['login'])) {
+    echo $twig->render('navbar.html.twig');
+    echo $twig->render('users.html.twig');
+} else {
+    echo $twig->render('navbar.html.twig', ['login' => $_SESSION['login']]);
+    echo $twig->render('settings.html.twig');
+}
